a8b03c6c85ce81af3575cddd2d5429bcf10e75a7
Wilson Ho <who@ualberta.ca>
2016-06-03 20:42:51 -0600

    Homepage Bug Fix

--- server.js
+++ server.js
@@ -57,10 +57,6 @@ app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use(express.static(path.join(__dirname, 'lib/css')));
 app.use('/', routes);
 
-app.on('stormpath.ready',function () {
-  console.log('Stormpath Ready');
-});
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
