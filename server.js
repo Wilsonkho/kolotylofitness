ea6530095c5568c6a6bce0892e0258b8549938d3
Wilson Ho <who@ualberta.ca>
2016-06-03 20:25:08 -0600

    initial website

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var http = require('http');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
+var path = require('path');
+var routes = require('./lib/routes');
 
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
@@ -9,6 +16,78 @@ http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+
+/**
+ * Application settings.
+ */
+app.set('trust proxy',true);
+app.set('view engine', 'jade');
+// app.set('views', './lib/views');
+app.set('views', path.join(__dirname, './lib/views'));
+app.locals.siteName = 'Kololyto Fitness';
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+
+/**
+ * Route initialization.
+ */
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'lib')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, 'lib/css')));
+app.use('/', routes);
+
+/**
+ * Route initialization.
+ */
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'lib')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, 'lib/css')));
+app.use('/', routes);
+
+app.on('stormpath.ready',function () {
+  console.log('Stormpath Ready');
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
